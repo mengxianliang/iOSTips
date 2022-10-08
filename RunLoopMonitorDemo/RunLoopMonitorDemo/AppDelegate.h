c75c0bf33126eac16e671ac82d25fcba99972742
孟宪亮 <mengxianliangok@163.com>
2022-10-08 17:24:15 +0800

    添加：运行时卡顿监控

---
+++ RunLoopMonitorDemo/RunLoopMonitorDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RunLoopMonitorDemo
+//
+//  Created by mxl on 2022/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RunLoopMonitorDemo/RunLoopMonitorDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RunLoopMonitorDemo
+//
+//  Created by mxl on 2022/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
